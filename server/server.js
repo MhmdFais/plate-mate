6364ed6dc1a0d94e7abab01e59461558deec5dd0
MhmdFais <117585798+MhmdFais@users.noreply.github.com>
2025-01-27 19:35:22 +0530

    Add Prisma setup with PostgreSQL integration and user models

--- server/server.js
+++ server/server.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const app = express();
 
-const PORT = 3000;
+const passport = require("passport");
+
+const loginRoute = require("./routes/loginRoute");
+const registerRouter = require("./routes/registerRoute");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(passport.initialize());
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
