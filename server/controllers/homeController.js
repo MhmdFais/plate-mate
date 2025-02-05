6df557e58ac7b73012f0aea8d3dc0e1ee77794cd
MhmdFais <117585798+MhmdFais@users.noreply.github.com>
2025-02-05 18:43:24 +0530

    feat: implement random recipe retrieval in home controller

--- server/controllers/homeController.js
+++ server/controllers/homeController.js
@@ -1,4 +1,20 @@
-const getRandomRecipe = async (req, res) => {};
+const getRandomRecipe = async (req, res) => {
+  try {
+    try {
+      const API = "https://api.spoonacular.com/recipes/complexSearch";
+    } catch (error) {
+      console.log(error);
+    }
+
+    const response = await fetch(API);
+
+    const data = await response.json();
+
+    res.json(data);
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 module.exports = {
   getRandomRecipe,
